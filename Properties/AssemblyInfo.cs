08da67e6ca7d1428b0d614e175105cc05cc2db11
Pavel Gyandzhuntsev <gpa@constructive.io>
2023-04-06 17:57:56 +0300

    Version change

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MicMute https://git.io/JeYVX")]
-[assembly: AssemblyDescription("https://github.com/Anc813/MicMute")]
+[assembly: AssemblyTitle("MicMuteBTControl")]
+[assembly: AssemblyDescription("https://github.com/pavelgz/MicMuteBTControl")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MicMute")]
+[assembly: AssemblyProduct("MicMuteBTControl")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.8.0")]
-[assembly: AssemblyFileVersion("0.0.8.0")]
+[assembly: AssemblyVersion("0.0.8.1")]
+[assembly: AssemblyFileVersion("0.0.8.1")]
 [assembly: NeutralResourcesLanguage("en")]
 
